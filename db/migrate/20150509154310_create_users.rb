8342feb76a8be8c3f64a638d0dfaad36a0e89229
Lex Evan <lexevan@icloud.com>
2015-05-09 12:13:18 -0400

    initial commit.

---
+++ db/migrate/20150509154310_create_users.rb
@@ -0,0 +1,27 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.string :company
+      t.string :website
+      t.string :location
+      
+      t.string :excited_about
+      t.date :experience
+      t.text :bio
+      t.string :expertise
+
+      t.string :linkedin
+      t.string :twitter
+      t.string :instagram
+
+      t.timestamps null: false
+    end
+
+      add_index :users, :username, unique: true
+
+  end
+end
